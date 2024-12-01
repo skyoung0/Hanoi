9b8a3efeb2baad11fc5a421ee25d00c1e2ce111a
skyoung0 <kenneth1000@naver.com>
2024-12-01 23:18:16 +0900

    예외처리, 재귀호출 횟수 계산, 에러처리 추가를 하여 cpp 언어를 활용한 프로그램 개선

--- cpptest.cpp
+++ cpptest.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
+using namespace std;
 
 // 하노이탑 알고리즘 함수
-void hanoi(int n, char from, char to, char aux) {
+int hanoi(int n, char from, char to, char aux) {
     if (n == 1) {
-        std::cout << "Move disk 1 from " << from << " to " << to << std::endl;
-        return;
+        cout << "Move disk 1 from " << from << " to " << to << endl;
+        return 1; // 재귀 호출 카운트
     }
-    hanoi(n - 1, from, aux, to); // 원반 n-1개를 보조 기둥(aux)으로 이동
-    std::cout << "Move disk " << n << " from " << from << " to " << to << std::endl;
-    hanoi(n - 1, aux, to, from); // 원반 n-1개를 목적 기둥(to)으로 이동
+    int steps = 0;
+    steps += hanoi(n - 1, from, aux, to); // 원반 n-1개를 보조 기둥(aux)으로 이동
+    cout << "Move disk " << n << " from " << from << " to " << to << endl;
+    steps += 1;
+    steps += hanoi(n - 1, aux, to, from); // 원반 n-1개를 목적 기둥(to)으로 이동
+    return steps; // 총 재귀 호출 횟수 반환
 }
 
 int main() {
-    int n = 3; // 원반 개수
-    std::cout << "Hanoi Tower solution for " << n << " disks:" << std::endl;
-    hanoi(n, 'A', 'C', 'B'); // A: 시작 기둥, C: 목적지 기둥, B: 보조 기둥
+    int n;
+    cout << "Enter the number of disks: ";
+    cin >> n;
+
+    // 입력값 검증
+    if (n <= 0) {
+        cout << "The number of disks must be greater than 0." << endl;
+        return 1;
+    }
+
+    cout << "\nHanoi Tower solution for " << n << " disks:\n" << endl;
+    int totalSteps = hanoi(n, 'A', 'C', 'B'); // A: 시작, C: 목적지, B: 보조
+    cout << "\nTotal steps: " << totalSteps << endl;
+
     return 0;
 }
